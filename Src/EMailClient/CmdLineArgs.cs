740460b66a6c2eb522d3e476c5c97112b2a6ce25
rdtek <mrrobindunn@gmail.com>
2016-07-18 11:37:58 +0800

    Added password reader and cmdlineargs classes.

---
+++ Src/EMailClient/CmdLineArgs.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MailClient
+{
+    class CmdLineArgs
+    {
+        private string[] m_originalArgs = null;
+        private Dictionary<string, string> m_argsDict 
+            = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+        public CmdLineArgs(string[] args)
+        {
+            m_originalArgs = args;
+            m_argsDict = ParseArgs(args);
+        }
+
+        public string Get(int argIndex)
+        {
+            return m_originalArgs[argIndex];
+        }
+
+        public bool Exists(int argIndex)
+        {
+            return m_originalArgs != null 
+                && m_originalArgs.Length >= argIndex + 1
+                && m_originalArgs[argIndex].Length >= 1;
+        }
+
+        private static Dictionary<string, string> ParseArgs(string[] args)
+        {
+            var dict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+
+            if (args == null || args.Length <= 0) return dict;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+                if (arg.IndexOf("/") == 0)
+                { /* found commad key */
+
+                    string command = arg; string parameter = "";
+
+                    if (args.Length >= i + 2)
+                    {
+                        string nextArg = args[i + 1];
+                        if (nextArg.IndexOf("/") == 0) { continue; }
+                        else { parameter = nextArg; }
+                    }
+                    dict.Add(command, parameter);
+                }
+            }
+
+            return dict;
+        }
+
+        public string GetArg(string argName, string defaultValue = "")
+        {
+            string result = defaultValue;
+
+            if (CheckDictKey(m_argsDict, argName))
+            {
+                result = m_argsDict[argName];
+            }
+
+            return result;
+        }
+
+        private static bool CheckDictKey(Dictionary<string, string> dict, string key)
+        {
+            return dict.ContainsKey(key) && dict[key].Length >= 1;
+        }
+
+        public override string ToString()
+        {
+
+            string result = "CmdLineArgs: { ";
+            foreach (KeyValuePair<string, string> kvp in m_argsDict)
+            {
+                result += string.Format("[Key = {0}, Value = {1}] ", kvp.Key, kvp.Value);
+            }
+            result += "}";
+
+            return result;
+        }
+    }
+
+}
--- Src/EMailClient/Program.cs
+++ Src/EMailClient/Program.cs
@@ -2,7 +2,7 @@
 using System.Net;
 using System.Net.Mail;
 
-namespace EMailClient
+namespace MailClient
 {
     //Code adapted from:
     //https://medium.com/developer-developers-developers/send-emails-through-office365-exchange-online-using-net-69b3a4a3b236#.t1vromdkg
@@ -16,61 +16,50 @@ namespace EMailClient
             string mailBody = "";
             string password = "";
 
+            var cmdArgs = new CmdLineArgs(args);
+
             //Get FROM address
-            if (args != null && args.Length >= 1) {
-                mailAddressFrom = args[0];
-            } else {
-                //No FROM address
-                Console.WriteLine("No FROM address");
-                return;
-            }
+            if(cmdArgs.Exists(0)) { mailAddressFrom = cmdArgs.Get(0); }
+            else { Console.WriteLine("No FROM address"); return; }
 
             //Get TO address
-            if (args != null && args.Length >= 2) {
-                mailAddressTo = args[1];
-            } else {
-                //No TO address
-                Console.WriteLine("No TO address");
-                return;
-            }
+            if (cmdArgs.Exists(1)) { mailAddressTo = cmdArgs.Get(1); }
+            else { Console.WriteLine("No TO address"); return; }
 
             //Get BODY
-            if (args != null && args.Length >= 3) {
-                mailBody = args[2];
-            } else {
-                //No BODY
-                Console.WriteLine("No BODY");
-                return;
-            }
+            if (cmdArgs.Exists(2)) { mailBody = cmdArgs.Get(2); }
+            else { Console.WriteLine("No BODY"); return; }
 
-            if(mailAddressFrom.Length >= 3 && mailAddressTo.Length >= 3 && mailBody.Length >= 1)
+            if(mailAddressFrom.Length >= 3 
+                && mailAddressTo.Length >= 3 
+                && mailBody.Length >= 1)
             {
                 Console.WriteLine("Enter password: ");
-                //TODO: hide inputted password chars
-                password = Console.ReadLine();
+                //Hide inputted password chars
+                password = PasswordReader.ReadPassword();
             }
 
-            MailMessage mail = new MailMessage(mailAddressFrom, mailAddressTo);
+            var mail = new MailMessage(mailAddressFrom, mailAddressTo);
             mail.IsBodyHtml = true;
             mail.Subject = "An email from Office365";
             mail.Body = "<html><body><p>" + mailBody + "</p></body></html>";
             
-            SmtpClient client = new SmtpClient("smtp.office365.com");
-            client.Port = 587;
-            client.EnableSsl = true;
-
+            var client = new SmtpClient {
+                Host = "smtp.office365.com",
+                Port = 587,
+                EnableSsl = true
+            };
+            
             // Important: This line of code must be executed before setting the NetworkCredentials 
             // object, otherwise the setting will be reset (a bug in .NET)
             client.UseDefaultCredentials = false; 
 
-            NetworkCredential cred = new NetworkCredential(mailAddressFrom, password);
+            var cred = new NetworkCredential(mailAddressFrom, password);
             client.Credentials = cred;
 
             Console.WriteLine("Sending mail...");
             client.Send(mail);
-
             Console.WriteLine("Mail sent.");
-            Console.ReadLine();
         }
     }
 }
